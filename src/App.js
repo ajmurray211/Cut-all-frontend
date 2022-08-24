19fffaf70034455967d890f01fe71f9b6efcb5c2
AJ Murray <aj.murr4y@gmail.com>
2022-08-24 13:45:02 -0400

    added a usageGraph and drawPart component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Home from './components/Main';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 import { Route, Routes} from 'react-router-dom';
 import logo from './Assets/cut-all-logo.png';
+import DrawPart from './components/drawPart/DrawPart';
+import UsageGraph from './components/usageGraph/UsageGraph';
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Nav horizontal='end' pills tabs className='nav'>
-          <NavItem active>
+        <Nav pills tabs>
+          <NavItem active className=''>
             <NavLink href='/'> Home </NavLink>
+            <NavLink href='/drawParts'> Draw Parts </NavLink>
+            <NavLink href='/usageGraph'> Usage Graph </NavLink>
           </NavItem>
         </Nav>
       </nav>
@@ -21,6 +25,8 @@ function App() {
       <div>
         <Routes>
           <Route path='/' element={<Home />}></Route>
+          <Route path='/drawParts' element={<DrawPart />}></Route>
+          <Route path='/usageGraph' element={<UsageGraph />}></Route>
         </Routes>
       </div>
     </div>
---
+++ src/components/usageGraph/UsageGraph.js
@@ -0,0 +1,7 @@
+import './usageGraph.css'
+
+const UsageGraph = () => {
+    return ( <h1>Usage page</h1>);
+}
+ 
+export default UsageGraph;
