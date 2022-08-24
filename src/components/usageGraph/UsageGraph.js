b96bcb364ad2cd025c1a700bc655c0698d0bd8ca
AJ Murray <aj.murr4y@gmail.com>
2022-08-24 18:52:59 -0400

    added a sorting button (no current function, put in filler graph data and some css

--- src/components/usageGraph/UsageGraph.js
+++ src/components/usageGraph/UsageGraph.js
@@ -1,4 +1,5 @@
 import './usageGraph.css'
+import { DropdownItem, Dropdown, DropdownToggle, DropdownMenu, Button } from 'reactstrap';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -7,6 +8,9 @@ const UsageGraph = () => {
     const [data, setData] = useState([])
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const toggle = () => setDropdownOpen((prevState) => !prevState);
+    const [activeSort, setActiveSort] = useState('')
 
     const API_URL = 'https://fast-meadow-65226.herokuapp.com/'
 
@@ -20,23 +24,29 @@ const UsageGraph = () => {
     }
 
     useEffect(() => {
-        // getData(`${API_URL}parts/?format=json`)
+        getData(`${API_URL}parts/?format=json`)
     }, [])
 
     const mockdata = [
-        { name: 'Page A', Kyle: 40, Rilyn: 24, Pat: 20, Gordon: 30, amt: 5 },
-        { name: 'Page B', Kyle: 10, Rilyn: 13, Pat: 20, Gordon: 30, amt: 20, },
-        { name: 'Page C', Kyle: 20, Rilyn: 56, Pat: 20, Gordon: 30, mt: 22, },
-        { name: 'Page D', Kyle: 27, Rilyn: 39, Pat: 20, Gordon: 30, amt: 20, },
-        { name: 'Page E', Kyle: 18, Rilyn: 48, Pat: 20, Gordon: 30, amt: 21, },
-        { name: 'Page F', Kyle: 23, Rilyn: 38, Pat: 20, Gordon: 30, amt: 25, },
-        { name: 'Page G', Kyle: 34, Rilyn: 43, Pat: 20, Gordon: 30, amt: 21, },
+        { name: 'Jan', Kyle: 40, Rilyn: 24, Pat: 20, Gordon: 30, amt: 5 },
+        { name: 'Feb', Kyle: 10, Rilyn: 13, Pat: 20, Gordon: 30, amt: 20, },
+        { name: 'Mar', Kyle: 20, Rilyn: 56, Pat: 20, Gordon: 30, mt: 22, },
+        { name: 'Apr', Kyle: 27, Rilyn: 39, Pat: 20, Gordon: 30, amt: 20, },
+        { name: 'May', Kyle: 18, Rilyn: 48, Pat: 20, Gordon: 30, amt: 21, },
+        { name: 'Jun', Kyle: 23, Rilyn: 38, Pat: 20, Gordon: 30, amt: 25, },
+        { name: 'Jul', Kyle: 34, Rilyn: 63, Pat: 20, Gordon: 30, amt: 21, },
+        { name: 'Aug', Kyle: 34, Rilyn: 43, Pat: 20, Gordon: 30, amt: 21, },
+        { name: 'Sep', Kyle: 34, Rilyn: 43, Pat: 20, Gordon: 30, amt: 21, },
+        { name: 'Oct', Kyle: 34, Rilyn: 43, Pat: 20, Gordon: 30, amt: 21, },
+        { name: 'Nov', Kyle: 34, Rilyn: 43, Pat: 20, Gordon: 30, amt: 21, },
+        { name: 'Dec', Kyle: 34, Rilyn: 43, Pat: 20, Gordon: 30, amt: 21, },
     ];
 
     const renderLineChart = (
         <LineChart
-            width={500}
-            height={300}
+            className='chart'
+            width={800}
+            height={600}
             data={mockdata}
             margin={{
                 top: 5,
@@ -58,11 +68,24 @@ const UsageGraph = () => {
         </LineChart>
     )
 
-    console.log(data)
+    const mappedParts = data.map((item) => {
+        return (
+            <DropdownItem action id={item.name} onClick={() => setActiveSort(item.name)} href=''>{item.name}</DropdownItem>
+        )
+    })
 
     return (
         <div className='container'>
-            <h1>Place holder for part search</h1>
+            <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                <DropdownToggle caret size="lg" dir='down'>
+                    Parts
+                </DropdownToggle>
+                <DropdownMenu>
+                    <DropdownItem onClick={() => setActiveSort('')}> Clear search</DropdownItem>
+                    {mappedParts}
+                </DropdownMenu>
+                {activeSort ? <Button id='sortedBy' disabled color='primary'>{activeSort}</Button> :<Button id='sortedBy' disabled color='warning'>No active search</Button>}
+            </Dropdown>
             {renderLineChart}
         </div>
     );
