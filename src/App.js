e1e4454e80733c5b60e44821607252afafc5c7f0
AJ Murray <aj.murr4y@gmail.com>
2022-08-24 19:51:49 -0400

    edited the nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './components/Main';
-import { Nav, NavItem, NavLink } from 'reactstrap';
-import { Route, Routes} from 'react-router-dom';
+import { Navbar, NavItem, NavLink, Nav, NavbarBrand, NavbarText } from 'reactstrap';
+import { Route, Routes } from 'react-router-dom';
 import logo from './Assets/cut-all-logo.png';
 import DrawPart from './components/drawPart/DrawPart';
 import UsageGraph from './components/usageGraph/UsageGraph';
@@ -9,7 +9,7 @@ import UsageGraph from './components/usageGraph/UsageGraph';
 function App() {
   return (
     <div className="App">
-      <nav>
+      {/* <nav>
         <Nav pills tabs>
           <NavItem active className=''>
             <NavLink href='/'> Home </NavLink>
@@ -17,11 +17,25 @@ function App() {
             <NavLink href='/usageGraph'> Usage Graph </NavLink>
           </NavItem>
         </Nav>
-      </nav>
+      </nav> 
       <div className="header-container">
-        <img className="cut-all-logo" src={logo}/>
+        <img className="cut-all-logo" src={logo} />
         <h2 style={{ fontSize: 40 }}>Inventory System</h2>
-      </div>
+      </div> */}
+
+      <Navbar>
+        <NavbarBrand href="/"><img className="cut-all-logo" alt='Cut all logo' src={logo} /></NavbarBrand>
+        <Nav className="me-auto" navbar>
+          <NavItem>
+            <NavLink href="/drawParts">Draw Parts</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/usageGraph">UsageGraph </NavLink>
+          </NavItem>
+        </Nav>
+        <NavbarText>Inventory System</NavbarText>
+      </Navbar>
+
       <div>
         <Routes>
           <Route path='/' element={<Home />}></Route>
