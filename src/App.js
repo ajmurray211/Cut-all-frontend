8e89889cf6b7e3568c150c9319bec58381e63c42
AJ Murray <aj.murr4y@gmail.com>
2022-09-30 18:44:44 -0400

    changed the nav bar linked from reactstrap links to react router dom links and mild css for those links

--- src/App.js
+++ src/App.js
@@ -10,31 +10,34 @@ import JobTIcket from './components/jobTicket/JobTicket';
 function App() {
   return (
     <div className="App">
-      {/* <Link to='/drawParts'>Test Link</Link> */}
       <Navbar color='primary'>
-        <NavbarBrand href="/"><img className="cut-all-logo" alt='Cut all logo' src={logo} /></NavbarBrand>
+        {/* <NavbarBrand href="/"><img className="cut-all-logo" alt='Cut all logo' src={logo} /></NavbarBrand> */}
+        <Link to="/"><img className="cut-all-logo" alt='Cut all logo' src={logo} /></Link>
         <Nav className="me-auto" navbar>
           <NavItem >
-            <NavLink href="/drawParts">Draw Parts</NavLink>
+            {/* <NavLink href="/drawParts">Draw Parts</NavLink> */}
+            <Link className='link' to='/drawParts'>Draw parts</Link>
           </NavItem>
           <NavItem>
             <NavLink href="/usageGraph" disabled>UsageGraph </NavLink>
+            {/* <Link className='link' to='/usageGraph'>Usage Graph</Link> */}
           </NavItem>
           <NavItem>
             <NavLink href="/jobTicket" disabled>Job Ticket </NavLink>
+            {/* <Link className='link' to='/jobTicket'>Job ticket</Link> */}
           </NavItem>
         </Nav>
         <NavbarText>Inventory System</NavbarText>
       </Navbar>
 
-      {/* <div>
+      <div>
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/drawParts' element={<DrawPart />}></Route>
           <Route path='/usageGraph' element={<UsageGraph />}></Route>
           <Route path='/jobTicket' element={<JobTIcket />}></Route>
         </Routes>
-      </div> */}
+      </div>
     </div>
   );
 }
