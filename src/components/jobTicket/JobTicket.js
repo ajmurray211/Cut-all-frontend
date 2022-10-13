b64dd440630d722da2f46fc114524e2a7414f511
AJ Murray <aj.murr4y@gmail.com>
2022-10-12 22:39:02 -0400

    created states for input fields and linked them. Entered the state values into the modal for signature page changed id names for input fields to reflect propper information

--- src/components/jobTicket/JobTicket.js
+++ src/components/jobTicket/JobTicket.js
@@ -7,7 +7,13 @@ import { useState } from 'react';
 const JobTIcket = () => {
     const [modal, setModal] = useState(false);
     const toggleModal = () => setModal(!modal);
-    let [ticketBody, setTicketBody] = useState([])
+    const [ticketBody, setTicketBody] = useState([])
+    const [worker, setWorker] = useState('')
+    const [billTo, setBillTo] = useState('')
+    const [truckNum, setTruckNum] = useState(null)
+    const [date, setDate] = useState(null)
+    const [address, setAddress] = useState(null)
+    const [jobInfo, setJobInfo] = useState([])
     let rowNum = 1
 
     const addRow = () => {
@@ -37,7 +43,7 @@ const JobTIcket = () => {
                                 name="select"
                                 placeholder="Who are you?"
                                 type="select"
-                                onChange={(event) => console.log(event.target.value)}
+                                onChange={(event) => setWorker(event.target.value)}
                             >
                                 <option></option>
                                 <option>Rilyn</option>
@@ -50,7 +56,7 @@ const JobTIcket = () => {
                     </Col>
                     <Col md={2}>
                         <Label>Truck Number:</Label>
-                        <Input type='number' onChange={(event) => console.log(event.target.value)}></Input>
+                        <Input type='number' onChange={(event) => setTruckNum(event.target.value)}></Input>
                     </Col>
                 </Row>
                 <Row>
@@ -65,19 +71,17 @@ const JobTIcket = () => {
                                 name="select"
                                 placeholder="Billing name"
                                 type="text"
-                                onChange={(event) => console.log(event.target.value)}
+                                onChange={(event) => setBillTo(event.target.value)}
                             ></Input>
                         </FormGroup>
                     </Col>
                     <Col md={3}>
                         <FormGroup>
-                            <Label for="employeeList">
+                            <Label for="otherWorkers">
                                 Other CA men on the job :
                             </Label>
                             <Input
-                                id="billingName"
-                                name="select"
-                                placeholder="Billing name"
+                                id="otherWorkers"
                                 type="checkbox"
                                 onChange={(event) => console.log(event.target.value)}
                             >
@@ -86,15 +90,13 @@ const JobTIcket = () => {
                     </Col>
                     <Col md={3}>
                         <FormGroup>
-                            <Label for="employeeList">
+                            <Label for="date">
                                 Date:
                             </Label>
                             <Input
-                                id="billingName"
-                                name="select"
-                                placeholder="Billing name"
+                                id="date"
                                 type="date"
-                                onChange={(event) => console.log(event.target.value)}
+                                onChange={(event) => setDate(event.target.value)}
                             >
                             </Input>
                         </FormGroup>
@@ -106,28 +108,25 @@ const JobTIcket = () => {
                     <Col md={4} />
                     <Col md={2}>
                         <FormGroup>
-                            <Label for="employeeList">
+                            <Label for="address">
                                 Address:
                             </Label>
                             <Input
-                                id="billingName"
-                                name="select"
-                                placeholder="Billing name"
+                                id="address"
+                                placeholder="Enter job address"
                                 type="text"
-                                onChange={(event) => console.log(event.target.value)}
+                                onChange={(event) => setAddress(event.target.value)}
                             >
                             </Input>
                         </FormGroup>
                     </Col>
                     <Col md={2}>
                         <FormGroup>
-                            <Label for="employeeList">
+                            <Label for="masterTicket">
                                 Master Ticket:
                             </Label>
                             <Input
-                                id="billingName"
-                                name="select"
-                                placeholder="Billing name"
+                                id="masterTicket"
                                 type="checkbox"
                                 onChange={(event) => console.log(event.target.value)}
                             >
@@ -161,18 +160,31 @@ const JobTIcket = () => {
             <Modal isOpen={modal}>
                 <ModalHeader toggle={toggleModal}>Signature form</ModalHeader>
                 <ModalBody>
-                    <h2> Standard job conditions:</h2>
-                    <ul>
-                        <li>1. Layout of work by others prior to arrival of operator.</li>
-                        <li>2. We cannot accept responsibility for damage to buried objects such as conduit, steel pipes, etc.</li>
-                        <li>3. All prices based on a maximum work height of 8’. Scaffolding supplied and erected by others unless prior arrangements are made.</li>
-                        <li>4. Water and power available within 150’ of work area. Parking for truck and power unit within 150’ of work area.</li>
-                        <li>5. Traffic control, water and/or dust partition by others.</li>
-                        <li>6. We reserve the right to bill all work on an hourly basis due to difficult working conditions and/or heavy reinforcing.</li>
-                        <li> 7. If for any reason are unable to work due to no fault of our own, then we must charge for stand-by time.</li>
-                        <li>8. Contractor is responsible for covering holes created by sawing and drilling.</li>
-                        <li>9. In the event of non-payment of any amount, when due, purchaser agrees to pay all collection costs including reasonable attorney fees.</li>
-                    </ul>
+                    <sction id='workerInput'>
+                        <h2>Job information:</h2>
+                        <ul id='inputContainer'>
+                            <li><span id='inputItem'>Who worked</span>:{worker}</li>
+                            <li><span id='inputItem'>Bill to</span>:{billTo}</li>
+                            <li><span id='inputItem'>Truck number</span>:{truckNum}</li>
+                            <li><span id='inputItem'>Date</span>:{date}</li>
+                            <li><span id='inputItem'>Address</span>:{address}</li>
+                            <li><span id='inputItem'>Work completed</span>:{jobInfo}</li>
+                        </ul>
+                    </sction>
+                    <section id='legalDisclaimer'>
+                        <h2> Standard job conditions:</h2>
+                        <ul>
+                            <li>1. Layout of work by others prior to arrival of operator.</li>
+                            <li>2. We cannot accept responsibility for damage to buried objects such as conduit, steel pipes, etc.</li>
+                            <li>3. All prices based on a maximum work height of 8’. Scaffolding supplied and erected by others unless prior arrangements are made.</li>
+                            <li>4. Water and power available within 150’ of work area. Parking for truck and power unit within 150’ of work area.</li>
+                            <li>5. Traffic control, water and/or dust partition by others.</li>
+                            <li>6. We reserve the right to bill all work on an hourly basis due to difficult working conditions and/or heavy reinforcing.</li>
+                            <li> 7. If for any reason are unable to work due to no fault of our own, then we must charge for stand-by time.</li>
+                            <li>8. Contractor is responsible for covering holes created by sawing and drilling.</li>
+                            <li>9. In the event of non-payment of any amount, when due, purchaser agrees to pay all collection costs including reasonable attorney fees.</li>
+                        </ul>
+                    </section>
 
                     <SignatureCanvas penColor='black' id='sign'
                         canvasProps={{ width: 425, height: 200, className: 'sigCanvas' }} />
