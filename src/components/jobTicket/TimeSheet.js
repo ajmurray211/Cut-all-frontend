9798996ff3eee5cf233384a0c9d191a4abdb7548
AJ Murray <aj.murr4y@gmail.com>
2023-01-20 12:39:02 -0500

    styles time sheets in css and minor edits to timesheet

--- src/components/jobTicket/TimeSheet.js
+++ src/components/jobTicket/TimeSheet.js
@@ -1,30 +1,45 @@
-import {Input, Table} from 'reactstrap';
+import { Input, Table, Col } from 'reactstrap';
 
 const TimeSheet = (props) => {
+    let mappedNames = ''
+
+    // console.log(props.namesList)
+    if (props.namesList) {
+        mappedNames = props.namesList.map((name) => {
+            return (
+                <Col md={4}>
+                    <Table bordered striped responsive hover >
+                        <caption >
+                            <h3>Time table for {name}</h3>
+                        </caption>
+                        <tbody>
+                            <tr>
+                                <th>Begin travel</th>
+                                <td><Input type='time' name='helperTravelBegin' id={name} onChange={props.handleChange}></Input></td>
+                            </tr>
+                            <tr>
+                                <th>End travel</th>
+                                <td><Input type='time' name='helperTravelEnd' id={name} onChange={props.handleChange}></Input></td>
+                            </tr>
+                            <tr>
+                                <th>Begin Job</th>
+                                <td><Input type='time' name='helperJobBegin' id={name} onChange={props.handleChange}></Input></td>
+                            </tr>
+                            <tr>
+                                <th>End job</th>
+                                <td><Input type='time' name='helperJobEnd' id={name} onChange={props.handleChange}></Input></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Col>
+            )
+        })
+    }
+
     return (
-        <Table bordered striped responsive hover >
-            <caption >
-                <h3>Time table for other CA workers on the job</h3>
-            </caption>
-            <tbody>
-                <tr>
-                    <th>Begin travel</th>
-                    <td><Input type='time' name='helperTravelBegin' id='helper' onChange={props.handleChange}></Input></td>
-                </tr>
-                <tr>
-                    <th>End travel</th>
-                    <td><Input type='time' name='helperTravelEnd' id='helper' onChange={props.handleChange}></Input></td>
-                </tr>
-                <tr>
-                    <th>Begin Job</th>
-                    <td><Input type='time' name='helperJobBegin' id='helper' onChange={props.handleChange}></Input></td>
-                </tr>
-                <tr>
-                    <th>End job</th>
-                    <td><Input type='time' name='helperJobEnd' id='helper' onChange={props.handleChange}></Input></td>
-                </tr>
-            </tbody>
-        </Table>
+        <section className='helperTimes'>
+            {mappedNames}
+        </section>
     );
 }
 
